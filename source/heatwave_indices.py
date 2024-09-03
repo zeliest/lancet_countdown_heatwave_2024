4b0641cfc742906bfdc989f82ad4f1ea2498ba43
zeliest <zelie.stalhandske@usys.ethz.ch>
2024-09-03 14:05:26 +0200

    remove unused functions

--- source/heatwave_indices.py
+++ source/heatwave_indices.py
@@ -5,152 +5,33 @@ import xarray as xr
 # which then lets use use dask and eventually have a full dask workflow, maybe even from
 # the raw data. 
 
-def heatwaves_counts_single_threshold(dataset_year, reference, days_threshold=3):
-    """
-    Accepts data as a (time, lat, lon) shaped boolean array.
-    Iterates through the array in the time dimension comparing the current
-    time slice to the previous one. For each cell, determines whether the
-    cell is True (i.e. is over the heatwave thresholds) and whether this is
-    the start, continuation, or end of a sequence of heatwave conditions.
-    Accumulates the number of occurances and counts the total occurances.
-    """
-    dataset_year = dataset_year.fillna(-9999)
-    dataset_year_asbool = (dataset_year > reference).values
-    
-    # Init arrays, pre allocate to (hopefully) improve performance.
-    out_shape: tuple = dataset_year_asbool.shape[1:]
-
-    last_slice: bool[:,:] = dataset_year_asbool[0, :, :]
-    curr_slice: bool[:,:] = dataset_year_asbool[0, :, :]    
-    hw_ends: bool[:,:] = np.zeros(out_shape, dtype=bool)
-    mask: bool[:,:] = np.zeros(out_shape, dtype=bool)
-    
-    # Init as int32 - value will never be > 365
-    accumulator = np.zeros(dataset_year_asbool.shape[1:], dtype=np.int32)
-    counter = np.zeros(dataset_year_asbool.shape[1:], dtype=np.int32)
-    
-    for i in range(1, dataset_year_asbool.shape[0]):
-        last_slice = dataset_year_asbool[i-1, :, :]
-        curr_slice = dataset_year_asbool[i, :, :]
-
-        # Add to the sequence length counter at all positions
-        # above threshold at prev time step using boolean indexing
-        accumulator[last_slice] += 1
-        
-        # End of sequence is where prev is true and current is false
-        # True where prev and not current
-        # Use pre-allicocated arrays for results
-        np.logical_and(last_slice, np.logical_not(curr_slice), out=hw_ends)
-        np.logical_and(hw_ends, (accumulator > days_threshold), out=mask)
-
-        # Add 1 where the sequences are ending and are > 3
-        counter[mask] += 1
-        # Reset the accumulator where current slice is empty
-        accumulator[np.logical_not(curr_slice)] = 0
-    
-    # Finally, 'close' the heatwaves that are ongoing at the end of the year
-    # End of sequence is where last value of iteration is true and accumulator is over given length
-    np.logical_and(curr_slice, (accumulator > days_threshold), out=mask)
-    
-    # Add the length of the accumulator where the sequences are ending and are > 3
-    counter[mask] += 1
-
-    # Convert np array to xr DataArray
-    counter = xr.DataArray(counter, 
-                            coords=[dataset_year.latitude.values,
-                                    dataset_year.longitude.values,
-                                   ], 
-                           dims=['latitude', 'longitude'],
-                           name='heatwave_count'
-                          )
-    
-    return counter
-
-
-def heatwaves_days_single_threshold(dataset_year, reference, days_threshold: int=3):
-    """
-    Accepts data as a (time, lat, lon) shaped boolean array.
-    Iterates through the array in the time dimension comparing the current
-    time slice to the previous one. For each cell, determines whether the
-    cell is True (i.e. is over the heatwave thresholds) and whether this is
-    the start, continuation, or end of a sequence of heatwave conditions.
-    Accumulates the number of days and counts the total lengths.
-    """
-    dataset_year = dataset_year.fillna(-9999)
-    dataset_year_asbool = (dataset_year > reference).values
-    
-    # pre allocate arrays
-    out_shape: tuple = dataset_year_asbool.shape[1:]
-
-    last_slice: bool[:,:] = dataset_year_asbool[0, :, :]
-    curr_slice: bool[:,:] = dataset_year_asbool[0, :, :]    
-    hw_ends: bool[:,:] = np.zeros(out_shape, dtype=bool)
-    mask: bool[:,:] = np.zeros(out_shape, dtype=bool)
-    
-    # Init as int32 - value will never be > 365
-    accumulator = np.zeros(out_shape, dtype=np.int32)
-    days = np.zeros(out_shape, dtype=np.int32)
-    
-    
-    for i in range(1, dataset_year_asbool.shape[0]):
-        last_slice = dataset_year_asbool[i-1, :, :]
-        curr_slice = dataset_year_asbool[i, :, :]
-
-        # Add to the sequence length counter at all positions
-        # above threshold at prev time step using boolean indexing
-        accumulator[last_slice] += 1
-        
-        # End of sequence is where prev is true and current is false
-        # True where prev and not current
-        # Use pre-allicocated arrays for results
-        np.logical_and(last_slice, np.logical_not(curr_slice), out=hw_ends)
-        np.logical_and(hw_ends, (accumulator > days_threshold), out=mask)
-
-        # Add the length of the accumulator where the sequences are ending and are > 3
-        days[mask] += accumulator[mask]
-        # Reset the accumulator where current slice is empty
-        accumulator[np.logical_not(curr_slice)] = 0
-    
-    # Finally, 'close' the heatwaves that are ongoing at the end of the year
-    # End of sequence is where last value of iteration is true and accumulator is over given length
-    np.logical_and(curr_slice, (accumulator > days_threshold), out=mask)
-    
-    # Add the length of the accumulator where the sequences are ending and are > 3
-    days[mask] += accumulator[mask]
-    
-    # Convert np array to xr DataArray
-    days = xr.DataArray(days, 
-                            coords=[dataset_year.latitude.values,
-                                    dataset_year.longitude.values,
-                                   ], 
-                           dims=['latitude', 'longitude'],
-                           name='heatwave_length'
-                          )
-    
-    return days
 
 
 def heatwaves_counts_multi_threshold(datasets_year, thresholds, days_threshold=2):
     """
-    Accepts data as a (time, lat, lon) shaped boolean array.
-    Iterates through the array in the time dimension comparing the current
-    time slice to the previous one. For each cell, determines whether the
-    cell is True (i.e. is over the heatwave thresholds) and whether this is
-    the start, continuation, or end of a sequence of heatwave conditions.
-    Accumulates the number of occurances and counts the total occurances.
+    Counts the number of heatwave occurrences above multiple thresholds.
+
+    Parameters:
+    - datasets_year (list of xarray.DataArray): A list of 3D arrays (time, lat, lon) 
+      containing the data for a single year. Each array corresponds to a different 
+      variable or condition that needs to be compared against a threshold.
+    - thresholds (list of float or xarray.DataArray): A list of threshold values or arrays 
+      against which the data is compared to identify heatwaves.
+    - days_threshold (int, optional): The minimum number of consecutive days required 
+      for an event to be considered a heatwave. Default is 2.
+
+    Returns:
+    - counter (xarray.DataArray): A 2D array of shape (lat, lon) where each value 
+      represents the count of heatwave occurrences that meet the days_threshold.
     """
     datasets_year = [d.fillna(-9999) for d in datasets_year]
-    # Init whole array to True
     threshold_exceeded = datasets_year[0] > thresholds[0]
-    # For each threshold array, 'and' them together
+    
     for dataset_year, thresh in zip(datasets_year[1:], thresholds[1:]):
-        # for each (data, threshold) pair, add constraint the threshold excedance array
         threshold_exceeded = np.logical_and(threshold_exceeded,
                                             dataset_year > thresh)
-    # Keep only the numpy array
     threshold_exceeded = threshold_exceeded.values
 
-    # Init arrays, pre allocate to (hopefully) improve performance.
     out_shape: tuple = threshold_exceeded.shape[1:]
 
     last_slice: bool[:, :] = threshold_exceeded[0, :, :]
@@ -158,72 +39,56 @@ def heatwaves_counts_multi_threshold(datasets_year, thresholds, days_threshold=2
     hw_ends: bool[:, :] = np.zeros(out_shape, dtype=bool)
     mask: bool[:, :] = np.zeros(out_shape, dtype=bool)
 
-    # Init as int32 - value will never be > 365
     accumulator = np.zeros(threshold_exceeded.shape[1:], dtype=np.int32)
     counter = np.zeros(threshold_exceeded.shape[1:], dtype=np.int32)
 
-    # Calculate the run length of the exceedances and count only the ones
-    # over the length threshold
     for i in range(1, threshold_exceeded.shape[0]):
         last_slice = threshold_exceeded[i - 1, :, :]
         curr_slice = threshold_exceeded[i, :, :]
 
-        # Add to the sequence length counter at all positions
-        # above threshold at prev time step using boolean indexing
         accumulator[last_slice] += 1
-
-        # End of sequence is where prev is true and current is false
-        # True where prev and not current
-        # Use pre-allicocated arrays for results
         np.logical_and(last_slice, np.logical_not(curr_slice), out=hw_ends)
         np.logical_and(hw_ends, (accumulator > days_threshold), out=mask)
 
-        # Add 1 where the sequences are ending and are > 3
         counter[mask] += 1
-        # Reset the accumulator where current slice is empty
         accumulator[np.logical_not(curr_slice)] = 0
 
-    # Finally, 'close' the heatwaves that are ongoing at the end of the year
-    # End of sequence is where last value of iteration is true and accumulator is over given length
     np.logical_and(curr_slice, (accumulator > days_threshold), out=mask)
-
-    # Add the length of the accumulator where the sequences are ending and are > 3
     counter[mask] += 1
 
-    # Convert np array to xr DataArray
     counter = xr.DataArray(counter,
                            coords=[datasets_year[0].latitude.values,
-                                   datasets_year[0].longitude.values,
-                                   ],
+                                   datasets_year[0].longitude.values],
                            dims=['latitude', 'longitude'],
-                           name='heatwave_count'
-                           )
+                           name='heatwave_count')
 
     return counter
 
 
 def heatwaves_days_multi_threshold(datasets_year, thresholds, days_threshold: int = 2):
     """
-    Accepts data as a (time, lat, lon) shaped boolean array.
-    Iterates through the array in the time dimension comparing the current
-    time slice to the previous one. For each cell, determines whether the
-    cell is True (i.e. is over the heatwave thresholds) and whether this is
-    the start, continuation, or end of a sequence of heatwave conditions.
-    Accumulates the number of days and counts the total lengths.
+    Counts the total number of days under heatwave conditions above multiple thresholds.
+
+    Parameters:
+    - datasets_year (list of xarray.DataArray): A list of 3D arrays (time, lat, lon) 
+      containing the data for a single year. Each array corresponds to a different 
+      variable or condition that needs to be compared against a threshold.
+    - thresholds (list of float or xarray.DataArray): A list of threshold values or arrays 
+      against which the data is compared to identify heatwaves.
+    - days_threshold (int, optional): The minimum number of consecutive days required 
+      for an event to be considered a heatwave. Default is 2.
+
+    Returns:
+    - days (xarray.DataArray): A 2D array of shape (lat, lon) where each value 
+      represents the total number of days under heatwave conditions.
     """
     datasets_year = [d.fillna(-9999) for d in datasets_year]
-    # Init array
     threshold_exceeded = datasets_year[0] > thresholds[0]
-    # For each threshold array, 'and' them together
+    
     for _data_year, _thresh in zip(datasets_year[1:], thresholds[1:]):
-        # for each (data, threshold) pair, add constraint the threshold excedance array
         threshold_exceeded = np.logical_and(threshold_exceeded,
                                             _data_year > _thresh)
 
-    # Keep only the numpy array
-    #threshold_exceeded = threshold_exceeded.values
-
-    # pre allocate arrays
     out_shape: tuple = threshold_exceeded.shape[1:]
 
     last_slice: bool[:, :] = threshold_exceeded[0, :, :]
@@ -231,7 +96,6 @@ def heatwaves_days_multi_threshold(datasets_year, thresholds, days_threshold: in
     hw_ends: bool[:, :] = np.zeros(out_shape, dtype=bool)
     mask: bool[:, :] = np.zeros(out_shape, dtype=bool)
 
-    # Init as int32 - value will never be > 365
     accumulator = np.zeros(out_shape, dtype=np.int32)
     days = np.zeros(out_shape, dtype=np.int32)
 
@@ -239,37 +103,20 @@ def heatwaves_days_multi_threshold(datasets_year, thresholds, days_threshold: in
         last_slice = threshold_exceeded[i - 1, :, :]
         curr_slice = threshold_exceeded[i, :, :]
 
-        # Add to the sequence length counter at all positions
-        # above threshold at prev time step using boolean indexing
         accumulator[last_slice] += 1
-
-        # End of sequence is where prev is true and current is false
-        # True where prev and not current
-        # Use pre-allicocated arrays for results
         np.logical_and(last_slice, np.logical_not(curr_slice), out=hw_ends)
         np.logical_and(hw_ends, (accumulator > days_threshold), out=mask)
 
-        # Add the length of the accumulator where the sequences are ending and are > 3
         days[mask] += accumulator[mask]
-        # Reset the accumulator where current slice is empty
         accumulator[np.logical_not(curr_slice)] = 0
 
-    # Finally, 'close' the heatwaves that are ongoing at the end of the year
-    # End of sequence is where last value of iteration is true and accumulator is over given length
     np.logical_and(curr_slice, (accumulator > days_threshold), out=mask)
-
-    # Add the length of the accumulator where the sequences are ending and are > 3
     days[mask] += accumulator[mask]
 
-    # Convert np array to xr DataArray
     days = xr.DataArray(days,
                         coords=[datasets_year[0].latitude.values,
-                                datasets_year[0].longitude.values,
-                                ],
+                                datasets_year[0].longitude.values],
                         dims=['latitude', 'longitude'],
-                        name='heatwaves_days'
-                        )
+                        name='heatwaves_days')
 
     return days
-
-
